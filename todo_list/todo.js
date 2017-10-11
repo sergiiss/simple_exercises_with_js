063ebe7e28ddb40f660f351e208b85034afae56c
Sergiiss <Dracula23@mail.ru>
2017-10-12 02:25:02 +0300

    Add execution and delete tasks

--- todo_list/todo.js
+++ todo_list/todo.js
@@ -1,12 +1,38 @@
+function doneTask() {
+  if (this.parentNode.style.textDecoration == 'line-through') {
+    this.parentNode.style.textDecoration = 'none';
+  }
+  else {
+    this.parentNode.style.textDecoration = 'line-through';
+  }
+}
+
+function destroyTask() {
+  document.getElementsByTagName('body')[0].removeChild(this.parentNode);
+}
+
 function addInput() {
   if (event.keyCode == 13) {
     var div = document.createElement('div');
     var elem = document.getElementById('list');
+    var check = document.createElement('input');
+    var destroy = document.createElement('div');
 
     div.className = "new";
-    div.innerHTML = "<input class='choose' type='checkbox'>" + "   " + elem.value;
+    check.className = "choose";
+    check.type = "checkbox";
+    destroy.className = "destroy";
 
+    // if (!document.getElementsByClassName('new')[0]) {
+
+    // }
+
+    div.innerHTML = check.outerHTML + "   " + elem.value + destroy.outerHTML;
     document.body.appendChild(div);
+
+    div.firstElementChild.addEventListener("click", doneTask);
+    div.lastElementChild.addEventListener("click", destroyTask);
+
     elem.value = '';
   }
 }
