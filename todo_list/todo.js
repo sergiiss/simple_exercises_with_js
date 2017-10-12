498d54d8c195e8166da75e8ad30d0498584ed692
Sergiiss <Dracula23@mail.ru>
2017-10-12 23:02:04 +0300

    Add quantity tasks

--- todo_list/todo.js
+++ todo_list/todo.js
@@ -1,14 +1,34 @@
 function doneTask() {
-  if (this.parentNode.style.textDecoration == 'line-through') {
-    this.parentNode.style.textDecoration = 'none';
+  if (getComputedStyle(this.parentNode).textDecorationLine == 'line-through') {
+    this.parentNode.classList.remove('through');
+    this.parentNode.classList.add('through-remove');
   }
   else {
-    this.parentNode.style.textDecoration = 'line-through';
+    this.parentNode.classList.add('through');
+    this.parentNode.classList.remove('through-remove');
   }
+  countTasks();
 }
 
 function destroyTask() {
-  document.getElementsByTagName('body')[0].removeChild(this.parentNode);
+  document.getElementById('input-field').removeChild(this.parentNode);
+  countTasks();
+
+  if (document.getElementById('bottom-list0').innerHTML == 0) {
+    document.body.removeChild(document.getElementById('bottom-list0'));
+    document.body.removeChild(document.getElementById('bottom-list1'));
+    document.body.removeChild(document.getElementById('bottom-list2'));
+  }
+}
+
+function countTasks() {
+  var counter = document.getElementsByClassName('through-remove').length;
+  document.getElementById('bottom-list0').innerHTML = "<span id='count-tasks'>" + counter + ' tasks left' + "</span>";
+
+  // var compled = document.getElementsByClassName('through');
+  // for (var i = 0; i < comled.length; ++i) {
+  //   compled[i].style.display = 'none';
+  // }
 }
 
 function addInput() {
@@ -19,16 +39,29 @@ function addInput() {
     var destroy = document.createElement('div');
 
     div.className = "new";
+    div.classList.add('through-remove');
     check.className = "choose";
     check.type = "checkbox";
     destroy.className = "destroy";
 
-    // if (!document.getElementsByClassName('new')[0]) {
+    if (!document.getElementsByClassName('new')[0]) {
+      var bottom0 = document.createElement('div');
+      var bottom1 = document.createElement('div');
+      var bottom2 = document.createElement('div');
 
-    // }
+      bottom0.id = "bottom-list0";
+      bottom1.id = "bottom-list1";
+      bottom2.id = "bottom-list2";
+
+      document.body.appendChild(bottom0);
+      document.body.appendChild(bottom1);
+      document.body.appendChild(bottom2);
+    }
 
     div.innerHTML = check.outerHTML + "   " + elem.value + destroy.outerHTML;
-    document.body.appendChild(div);
+    document.getElementById('input-field').appendChild(div);
+
+    countTasks();
 
     div.firstElementChild.addEventListener("click", doneTask);
     div.lastElementChild.addEventListener("click", destroyTask);
